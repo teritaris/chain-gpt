b4fc4bda8119963e30ff99939db2fc56291f34f2
teria <ribonode@gmail.com>
2023-03-27 03:48:04 +0900

    ルーティング修正

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import {chat} from "./routes/chat";
+import * as path from "path"
 
 // とりあえず落ちないようにする
 process.on('uncaughtException', function(err) {
@@ -18,9 +19,16 @@ app.listen(C.conf.web.port, C.conf.web.listen, () => {
 });
 
 app.get("/", (req, res) => {
-  res.json({ j: "<●>_<●>" });
+  res.sendFile(path.join(__dirname, "/../../frontend/index.html"))
+});
+app.get("/chat", (req, res) => {
+  res.sendFile(path.join(__dirname, "/../../frontend/ask/chat.html"))
+});
+app.get("/training", (req, res) => {
+  res.sendFile(path.join(__dirname, "/../../frontend/personality/setting.html"))
 });
 
 app.use('/chat', chat);
 
+// 画像読むのに必要
 app.use(express.static('../frontend'));
